8c5564739036695ef405061ad095bc598aa88b37
Regis Ximendes <regisximendes.dev@gmail.com>
2020-06-17 13:19:09 -0300

    change routes geto post

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,14 +1,14 @@
 module.exports = (application) => {
 
-    application.get('/user/timeline', (req, res, next) => {
+    application.post('/user/timeline', (req, res, next) => {
         application.src.controller.twitterController.getUserTimeline(application, req, res)
     });
 
-    application.get('/user/home/:username', (req, res, next) => {
+    application.post('/user/home/:username', (req, res, next) => {
         application.src.controller.twitterController.getUserHome(application, req, res)
     });
 
-    application.get('/search/:query', (req, res, next) => {
+    application.post('/search/:query', (req, res, next) => {
         application.src.controller.twitterController.search(application, req, res)
     });
 }
